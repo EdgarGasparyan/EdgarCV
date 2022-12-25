6118d44ee8dc688115d2e07a46ba9caf852ebfda
EdgarGasparyan <el22092002@gmail.com>
2022-12-25 23:00:29 +0400

    add online shop

--- src/components/data.js
+++ src/components/data.js
@@ -8,54 +8,84 @@ import Evitep from "../assets/Evitep.png";
 import To_do_list from "../assets/To_do_list.png";
 import Rondom_number from "../assets/Rondom-number.png";
 import xplode from "../assets/xplode.png";
-import ADVENTURE from "../assets/ADVENTURE.jpg"
-import Solar from "../assets/Solar-System.jpg"
+import ADVENTURE from "../assets/ADVENTURE.jpg";
+import Solar from "../assets/Solar-System.jpg";
+import Online_shop from "../assets/Online_shop.jpg";
+import { nanoid } from "nanoid";
 
 export const data = [
   {
-    id: 1,
+    id: nanoid(),
+    image: Online_shop,
+    title: "Online Shop",
+    github: "https://github.com/EdgarGasparyan/Shop-Front",
+    demo: "https://edgargasparyan.github.io/Shop-Front/",
+  },
+  {
+    id: nanoid(),
+    image: Solar,
+    title: "Nasa API task",
+    github: "https://github.com/EdgarGasparyan/nasaTask",
+    demo: "https://edgargasparyan.github.io/nasaTask",
+  },
+  {
+    id: nanoid(),
+    image: Rondom_number,
+    title: "Get rondom number",
+    github: "https://github.com/EdgarGasparyan/Random_number_test",
+    demo: "https://edgargasparyan.github.io/Solicy_test/",
+  },
+  {
+    id: nanoid(),
+    image: xplode,
+    title: "Pagination react js",
+    github: "https://github.com/EdgarGasparyan/xplode",
+    demo: "http://edgargasparyan.github.io/xplode",
+  },
+  {
+    id: nanoid(),
     image: GoCorona,
     title: "Responsive Layout",
     github: "https://github.com/EdgarGasparyan/GoCorona-web-Adaptive-layout",
     demo: "https://edgargasparyan.github.io/GoCorona-web-Adaptive-layout/",
   },
   {
-    id: 2,
+    id: nanoid(),
     image: Responsive,
     title: "Responsive Layout",
     github: "https://github.com/EdgarGasparyan/Responsive-layout",
     demo: "https://edgargasparyan.github.io/Responsive-layout/",
   },
   {
-    id: 3,
+    id: nanoid(),
     image: Slider,
     title: "Slider",
     github: "https://github.com/EdgarGasparyan/Slider",
     demo: "https://edgargasparyan.github.io/Slider/",
   },
   {
-    id: 4,
+    id: nanoid(),
     image: NWTASK,
     title: "Get Rondom Cats",
     github: "https://github.com/EdgarGasparyan/NWTASK",
     demo: "https://edgargasparyan.github.io/NWTASK/",
   },
   {
-    id: 5,
+    id: nanoid(),
     image: HightChart,
     title: "HightChart",
     github: "https://github.com/EdgarGasparyan/HightChart",
     demo: "https://edgargasparyan.github.io/HightChart/",
   },
   {
-    id: 6,
+    id: nanoid(),
     image: GetTotalCoin,
     title: "Get Total Coin",
     github: "https://github.com/EdgarGasparyan/getTotalCoinTSX",
     demo: "https://edgargasparyan.github.io/getTotalCoinTSX/",
   },
   {
-    id: 7,
+    id: nanoid(),
     image: Evitep,
     title: "Layout",
     github: "https://github.com/EdgarGasparyan/Evistep_task",
@@ -69,31 +99,10 @@ export const data = [
     demo: "https://edgargasparyan.github.io/Ed-s-Todo-List/",
   },
   {
-    id: 9,
-    image: Rondom_number,
-    title: "Get rondom number",
-    github: "https://github.com/EdgarGasparyan/Random_number_test",
-    demo: "https://edgargasparyan.github.io/Solicy_test/",
-  },
-  {
-    id: 10,
-    image: xplode,
-    title: "Pagination react js",
-    github: "https://github.com/EdgarGasparyan/xplode",
-    demo: "http://edgargasparyan.github.io/xplode",
-  },
-  {
-	id: 11,
-	image: ADVENTURE,
-	title: "Layout",
-	github: "https://github.com/EdgarGasparyan/ADVENTURE",
-	demo: "https://edgargasparyan.github.io/ADVENTURE",
- },
- {
-	id: 12,
-	image: Solar,
-	title: "Nasa API task",
-	github: "https://github.com/EdgarGasparyan/nasaTask",
-	demo: "https://edgargasparyan.github.io/nasaTask",
- },
+    id: nanoid(),
+    image: ADVENTURE,
+    title: "Layout",
+    github: "https://github.com/EdgarGasparyan/ADVENTURE",
+    demo: "https://edgargasparyan.github.io/ADVENTURE",
+  }
 ];
